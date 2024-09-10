17780fac5e4fd43c953e53133b799246a6ff54dd
DEIBYMP <deibymuoz2002@gmail.com>
2024-09-10 15:43:49 -0500

    Ajuste de resolucion

--- mp_pose_video.py
+++ mp_pose_video.py
@@ -4,16 +4,20 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-#cap = cv2.VideoCapture("media\Pausa Activa con Ejercicios Combinados - Trim.mp4")
+cap = cv2.VideoCapture("media/5262396-uhd_3840_2160_25fps.mp4")
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+#cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+window_width = 1270
+window_height = 720
 
 with mp_pose.Pose(static_image_mode=False) as pose:
     while True:
         ret, frame = cap.read()
         if ret == False:
             break
-        frame = cv2.flip(frame, 1)
+        frame = cv2.resize(frame, (window_width, window_height))
+        #frame = cv2.flip(frame, 1)
         height, width, _ = frame.shape
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = pose.process(frame_rgb)
@@ -25,7 +29,7 @@ with mp_pose.Pose(static_image_mode=False) as pose:
                 mp_drawing.DrawingSpec(color=(255,255,255), thickness=2)
             )
 
-        cv2.imshow("Frame", frame)
+        cv2.imshow("Frame", frame) 
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
